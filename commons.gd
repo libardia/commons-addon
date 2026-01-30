e7fa89b23792533ec95debf6f59005a4ea5eb6f3
Anthony Libardi <libardi42@gmail.com>
2026-01-30 00:07:33 -0500

    make member

--- commons.gd
+++ commons.gd
@@ -3,10 +3,11 @@ extends EditorPlugin
 
 
 const INPUT_DETECTOR := "InputDetector"
+var addon_path: String
 
 
 func _enable_plugin() -> void:
-    var addon_path: String = get_script().get_path().get_base_dir()
+    addon_path = get_script().get_path().get_base_dir()
     add_autoload_singleton(INPUT_DETECTOR, addon_path.path_join("globals/input_detector.gd"))
 
 
