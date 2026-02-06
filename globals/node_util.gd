c84e5ea63367eadaf925e1574d6fe43cb5255ea7
Anthony Libardi <libardi42@gmail.com>
2026-02-06 15:24:05 -0500

    add node util

---
+++ globals/node_util.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+func absolute_z(node: Node2D) -> int:
+    var working = node
+    var z_index = 0
+    while working and working is Node2D:
+        z_index += working.z_index
+        if not working.z_as_relative:
+            break
+        working = working.get_parent()
+    return z_index
