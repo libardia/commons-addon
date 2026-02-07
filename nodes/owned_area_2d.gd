6504f3a866d850f9f82690076fcc8515c4fb76f4
Anthony Libardi <libardi42@gmail.com>
2026-02-07 16:30:32 -0500

    owned_by is set on enter_tree if null

--- nodes/owned_area_2d.gd
+++ nodes/owned_area_2d.gd
@@ -3,5 +3,9 @@ extends Area2D
 
 
 ## The node to which this Area2D belongs. If null, this will be assumed to be its parent.
-@export var owned_by: Node2D:
-    get: return owned_by if owned_by else get_parent()
+@export var owned_by: Node2D
+
+
+func _enter_tree() -> void:
+    if not owned_by:
+        owned_by = get_parent()
