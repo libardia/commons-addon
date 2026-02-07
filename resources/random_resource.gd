326a52c533b372525b4d571fe671c3cca220c65b
Anthony Libardi <libardi42@gmail.com>
2026-02-07 15:37:34 -0500

    doc comments, and choosing recursively is the default now

--- resources/random_resource.gd
+++ resources/random_resource.gd
@@ -5,6 +5,16 @@ extends Resource
 @export var choices: Array[Resource]
 
 
+## Choose one of the options at random, but if the result is also a RandomResource, choose again,
+## and continue until a non-RandomResource is found.
 func choose(rng: RandomNumberGenerator = null) -> Resource:
+    var r = choose_once(rng)
+    while r is RandomResource:
+        r = r.choose_once(rng)
+    return r
+
+
+## Choose one of the options at random, with equal probability.
+func choose_once(rng: RandomNumberGenerator = null) -> Resource:
     var i = rng.randi() if rng else randi()
     return choices[i % choices.size()]
--- resources/weighted_random_resource.gd
+++ resources/weighted_random_resource.gd
@@ -5,14 +5,15 @@ extends RandomResource
 @export var weights: Array[float]
 
 
-func choose(rng: RandomNumberGenerator = null) -> Resource:
+## Choose one of the options at random, weighted corresponding to the weights array.
+func choose_once(rng: RandomNumberGenerator = null) -> Resource:
     assert(weights.size() == choices.size(), "'choices' and 'weights' must be the same length.")
     var f = rng.randf() if rng else randf()
     var result = f * weights.reduce(func(acc, elem): return acc + elem, 0)
     var cursor = 0.0
     for i in choices.size():
         cursor += weights[i]
-        if result < cursor:
+        if result <= cursor:
             return choices[i]
     assert(false, "'choose' of WeightedRandomResource never found a choice!")
     return null
