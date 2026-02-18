9a8f495d79dbb683a1750155a7b79c369870844d
Anthony Libardi <libardi42@gmail.com>
2026-02-18 13:39:18 -0500

    UILayer can show the UI in the editor if desired

--- nodes/ui_layer.gd
+++ nodes/ui_layer.gd
@@ -1,10 +1,30 @@
+@tool
 class_name UILayer
 extends CanvasLayer
 
 
-@export var ui_scene: PackedScene
+@export var ui_scene: PackedScene:
+    set(value):
+        ui_scene = value
+        _refresh()
+
+@export var show_in_editor: bool = false:
+    set(value):
+        show_in_editor = value
+        _refresh()
+
+var _ui_instance: Node
 
 
 func _enter_tree() -> void:
-    if ui_scene:
-        add_child(ui_scene.instantiate())
+    _refresh()
+
+
+func _refresh() -> void:
+    # not (e and not s) = not e or s
+    var should_show := not Engine.is_editor_hint() or show_in_editor
+    if should_show and not _ui_instance:
+        _ui_instance = ui_scene.instantiate()
+        add_child(_ui_instance)
+    elif not should_show and _ui_instance:
+        _ui_instance.queue_free()
