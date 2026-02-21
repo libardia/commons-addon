b6ed64158074ddec7bb5e3dd111de39dd8f06446
Anthony Libardi <libardi42@gmail.com>
2026-02-21 17:25:37 -0500

    Only change icon if the property is set AFTER the node is ready; this allows changing in real-time when in the editor, but will not recreate and reset the icon when first created

--- editor_only/nodes/button_editor_icon.gd
+++ editor_only/nodes/button_editor_icon.gd
@@ -4,6 +4,6 @@ extends Button
 
 @export var icon_name: StringName:
     set(value):
+        icon_name = value
         if is_node_ready():
-            icon_name = value
             icon = EditorIcons.by_name(icon_name)
--- editor_only/nodes/texture_rect_editor_icon.gd
+++ editor_only/nodes/texture_rect_editor_icon.gd
@@ -5,4 +5,5 @@ extends TextureRect
 @export var icon_name: StringName:
     set(value):
         icon_name = value
-        texture = EditorIcons.by_name(icon_name)
+        if is_node_ready():
+            texture = EditorIcons.by_name(icon_name)
